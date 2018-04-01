bf6f5226848805fbe8c38d20141f52fa8177bcc6
CurryYuan <1172248391@qq.com>
2018-04-01 23:27:13 +0800

    Submit

---
+++ homework2/Solar System/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float speed;
+    float rz, rx, ry;
+    // Use this for initialization  
+    void Start()
+    {
+        speed = Random.Range(10, 40);
+        rx = Random.Range(0, 20);
+        ry = Random.Range(0, 20);
+        rz = Random.Range(0, 20);
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+        Vector3 axis = new Vector3(0, ry, rz);
+        this.transform.RotateAround(new Vector3(0, 0, 0), axis, speed * Time.deltaTime);
+    }
+}
---
+++ homework2/Solar System/Assets/Scripts/NewBehaviourScript1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+    public Transform origin;
+    public float speed;
+    float rz, rx, ry;
+    // Use this for initialization  
+    void Start()
+    {
+        speed = Random.Range(10, 40);
+        rx = Random.Range(0, 20);
+        ry = Random.Range(0, 20);
+        rz = Random.Range(0, 20);
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+        Vector3 axis = new Vector3(0, ry, rz);
+        this.transform.RotateAround(origin.position, axis, speed * Time.deltaTime);
+    }
+}
